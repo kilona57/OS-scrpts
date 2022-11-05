24974bbece55e80da3d45ebe0774efdde82d566e
kilona57 <113191278+kilona57@users.noreply.github.com>
2022-11-05 13:19:27 +0300

    Delete z_7.sh

--- z_7.sh
+++
@@ -1,8 +0,0 @@
-#! usr/bin/bash
-if [ ! -d $3 ]; then
-    echo "$0: Error opening dir: $3"
-else
-    FIND_CMD="find $3 -type f"
-    $FIND_CMD -size +$1c -size -$2c -printf "%h, %f, %s\n" > file.txt
-fi
-exit 0
